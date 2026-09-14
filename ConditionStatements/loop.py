a7e1fa5b4c764e558a731b83aba4f2aeb7d8aafb
AginKK <aknmdlearn24@gmail.com>
2026-09-14 17:06:19 +0530

    loop patterns

--- ConditionStatements/loop.py
+++ ConditionStatements/loop.py
@@ -4,5 +4,16 @@
 # for i in range(0,11,2):
 #     print(i)
 
+# for i in range(5):
+#     print("*")
+
+# for i in range(5):
+#     print("* "*5)
+
+# for i in range(5):
+#     print("*",end="    ")
+
 for i in range(5):
-    print("*")
+    for j in range(5):
+        print(j,end="")
+    print()
